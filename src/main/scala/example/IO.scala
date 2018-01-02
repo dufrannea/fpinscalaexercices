2494e4d5aabf5d9aea33bdff030aa4b5a4210b67
Arnaud Dufranne <a.dufranne@criteo.com>
2018-01-02 18:03:48 +0100

    IO

--- src/main/scala/example/IO.scala
+++ src/main/scala/example/IO.scala
@@ -2,22 +2,54 @@ package IO
 
 import Monad.Monad
 
-trait IO[A] { self => 
+trait NaiveIO[A] { self => 
   def run() : A
-  def flatMap[B](f : A => IO[B])(b : IO[A]) : IO[B] = new IO[B] {
-    def run() : B = f(self.run()).run()
+  def flatMap[B](f : A => NaiveIO[B])(b : NaiveIO[A]) : NaiveIO[B] = new NaiveIO[B] {
+    def run(): B = f(self.run()).run()
   }
-  def map[B](f : A => B)(b : IO[B]) : IO[B] = new IO[B]{
+  def map[B](f : A => B)(b : NaiveIO[B]) : NaiveIO[B] = new NaiveIO[B]{
     def run() = f(self.run())
   }
 }
 
 object Instances  {
-  val ioMonad = new Monad[IO] {
-    override def unit[A](a : A) : IO[A] = new IO[A]{ def run() : A = a }
+  val ioMonad = new Monad[NaiveIO] {
+    override def unit[A](a : A): NaiveIO[A] = new NaiveIO[A] {
+      def run() : A = a 
+    }
 
-    override def flatMap[A, B](f: A => IO[B])(a: IO[A]) : IO[B] = {
+    override def flatMap[A, B](f: A => NaiveIO[B])(a: NaiveIO[A]): NaiveIO[B] = {
       f(a.run())
     }
   }
 }
+
+trait IO[A] { self =>
+  def flatMap[B](f: A => IO[B]): IO[B] = FlatMap(self, f)
+
+  @annotation.tailrec final def run[A](io: IO[A]): A = io match {
+    case Return(a) => a
+    case Delay(thunk) => thunk()
+    case Suspend(f) => run(f())
+    case FlatMap(a, f) => {
+      val torun = a match {
+        case Return(ap) => f(ap)
+        case Delay(thunk) => f(thunk())
+        case Suspend(fs) => fs().flatMap(f)
+        case FlatMap(ap, fp) => ap.flatMap(x => fp(x).flatMap(f))
+      }
+      run(torun)
+    }
+  }
+}
+
+object IO {
+  def apply[A](f: () => A) = {
+    Suspend(() => Return(f()))
+  }
+}
+
+case class Return[A](a: A) extends IO[A]
+case class Delay[A](thunk: () => A) extends IO[A]
+case class Suspend[A](f: () => IO[A]) extends IO[A]
+case class FlatMap[A,B](a: IO[A], f: A => IO[B]) extends IO[B]
